c2eae4b224f3d71301da25642c584badbc6ee307
thomasLike <dandelionpjh@gmail.com>
2025-07-03 15:07:15 +0800

    some os files

---
+++ OS_from_scratch/src/main.asm
@@ -0,0 +1,43 @@
+ORG 0x7C00
+BITS 16
+
+main:
+	mov ax, 0
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+
+	mov sp, 0X7C00
+	mov si, os_boot_msg
+	call print
+	HLT
+
+halt:
+	JMP halt
+
+print:
+	push si
+	push ax
+	push bx
+
+print_loop:
+	LODSB
+	OR al, al
+	JZ done_print
+
+	mov ah, 0x0e
+	mov bh, 0
+	int 0x10
+
+	JMP print_loop
+
+done_print:
+	pop bx
+	pop ax
+	pop si
+	ret
+	
+os_boot_msg: db 'first OS has booted!', 13, 10, 0
+
+TIMES 510-($-$$) DB 0
+DW 0AA55h
