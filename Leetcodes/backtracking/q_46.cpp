b5449946472b0876336c701a91ce48022b38d35d
george <reborn.leisure@gmail.com>
2025-07-10 14:02:22 +0800

    update leetcode 20250710

--- Leetcodes/backtracking/q_46.cpp
+++ Leetcodes/backtracking/q_46.cpp
@@ -33,24 +33,22 @@ Constraints:
 
 class Solution {
 private:
-    void back_tracking(vector<int>& nums, std::vector<std::vector<int>>& ans, int i, std::vector<int>&& curr) {
-        if (curr.size() == nums.size()) {
-            ans.push_back(curr);
+    void back_tracking(vector<int>& nums, std::vector<std::vector<int>>& ans, int i) {
+        if (i == nums.size()) {
+            ans.push_back(nums);
             return;
         }
 
-        for (int j = 0; j < nums.size(); ++j) {
-
-            if (std::find(curr.begin(), curr.end(), nums[j]) == curr.end()) {
-                curr.push_back(nums[j]);
-                back_tracking(nums, ans, j + 1, std::move(curr));
-                curr.pop_back();
-            }
+        for (int j = i; j < nums.size(); ++j) {
+            std::swap(nums[i], nums[j]);
+            back_tracking(nums, ans, i + 1);
+            std::swap(nums[i], nums[j]);
         }
     }
     vector<vector<int>> by_back_tracking(vector<int>& nums) {
         std::vector<std::vector<int>> ans;
-        back_tracking(nums, ans, 0, std::vector<int>());      
+        std::vector<int> curr;
+        back_tracking(nums, ans, 0);      
         
         return ans;
     }
@@ -59,34 +57,3 @@ public:
         return by_back_tracking(nums);
     }
 };
-
-/*
-class Solution {
-public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        std::vector<std::vector<int>> result;
-        std::vector<int> curr;
-        backtrack(curr, nums, result);
-
-        return result;
-    }
-
-private:
-
-    void backtrack(std::vector<int>& curr, std::vector<int>& nums, std::vector<std::vector<int>>& result) {
-
-        if (curr.size() == nums.size()) {
-            result.push_back(curr);
-            return;
-        }
-
-        for (size_t i = 0; i < nums.size(); ++i) {
-            if (std::find(curr.begin(), curr.end(), nums[i]) == curr.end()) {
-                curr.push_back(nums[i]);
-                backtrack(curr, nums, result);
-                curr.pop_back();
-            }
-        }
-    }
-};
-*/
